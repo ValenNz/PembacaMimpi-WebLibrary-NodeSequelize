e6534de9b431858a066327136451640613d86390
ValenNz <nuevalenra05@gmail.com>
2023-01-24 06:11:05 +0700

    add contrroller multer

--- server.js
+++ server.js
@@ -14,14 +14,18 @@ const cors = require(`cors`)
 
 /** open CORS policy */ 
 app.use(cors()) // Menghubungkan broweser ke web-service
+app.use(express.static(__dirname))
 
 /* Mengimport semua router */
-const memberRoute = require(`./routes/member.routes`)
+const memberRoute = require(`./routes/member.routers`)
 const adminRoute = require('./routes/admin.routers')    
+const bookRoute = require(`./routes/book.routers`)
 
 /* Mendefinisikan path sebelum router */
 app.use(`/member`, memberRoute) // roueter member -> localhost:8000/member
 app.use('/admin', adminRoute)
+app.use(`/book`, bookRoute)
+
 /* Menjalankan server di port 8000 */
 app.listen(PORT, () => {
     console.log(`Server pembaca mimpi berjalan di port ${PORT}`)
